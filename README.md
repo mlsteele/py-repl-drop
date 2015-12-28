1afb97d59d9cdb12f8f62b13ff368f937fd52a74
Miles Steele <miles@milessteele.com>
2015-12-27 22:36:45 -0500

    add example

--- README.md
+++ README.md
@@ -4,4 +4,15 @@ Function to drop into a python repl real quick.
 The cool thing about this is that globals and local variables from the caller function are available in the repl.
 Press `^D` to return control to the program.
 
-Copy the interact function from `repldrop.py` and happy REPLing!
+Copy the `interact` function from `repldrop.py` and happy REPLing!
+
+```python
+def interact():
+    """Drop to an interactive REPL."""
+    import code, inspect
+    frame = inspect.currentframe()
+    prevlocals = frame.f_back.f_locals
+    replvars = globals().copy()
+    replvars.update(prevlocals)
+    code.InteractiveConsole(locals=replvars).interact()
+```
--- repldrop.py
+++ repldrop.py
@@ -6,3 +6,37 @@ def interact():
     replvars = globals().copy()
     replvars.update(prevlocals)
     code.InteractiveConsole(locals=replvars).interact()
+
+
+EXAMPLE_GLOBAL_VAR = {"a": "b"}
+
+def foo(n):
+    """An example function for debugging."""
+    if n == 0:
+        return
+    a = n + 1
+    b = 2 * a
+    if n == 1:
+        interact()
+    foo(n-1)
+
+
+if __name__ == "__main__":
+    foo(5)
+    print "done."
+
+"""
+$ python repldrop.py
+Python 2.7.10 (default, Dec  5 2015, 03:33:07)
+[GCC 5.2.1 20151010] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+(InteractiveConsole)
+>>> EXAMPLE_GLOBAL_VAR
+{'a': 'b'}
+>>> b
+4
+>>> n
+1
+>>> ^D
+done.
+"""
